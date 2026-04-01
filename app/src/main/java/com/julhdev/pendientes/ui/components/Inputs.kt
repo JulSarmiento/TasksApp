bcae62b176f7debbc060b4a2fab7a87275fe4a04
JulSarmiento <jasa1999@hotmail.com>
2026-04-01 18:37:50 -0500

    feat: se terminan las correcciones

--- app/src/main/java/com/julhdev/pendientes/ui/components/Inputs.kt
+++ app/src/main/java/com/julhdev/pendientes/ui/components/Inputs.kt
@@ -40,12 +40,12 @@ import com.julhdev.pendientes.ui.theme.NeonCyan
  */
 @Composable
 fun InputText(
-    modifier: Modifier = Modifier,
-    placeholder: String,
-    value: String,
-    onValueChange: (String) -> Unit,
-    showInputDialog: MutableState<Boolean>,
-    action: () -> Unit = {},
+  modifier: Modifier = Modifier,
+  placeholder: String,
+  value: String,
+  onValueChange: (String) -> Unit,
+  showInputDialog: MutableState<Boolean>,
+  action: () -> Unit = {},
   ) {
 
   val setError = remember { mutableStateOf(false) }
@@ -86,7 +86,7 @@ fun InputText(
         },
         singleLine = true,
         modifier = Modifier
-          .weight(1f) // 👈 importante
+          .weight(1f)
           .background(
             color = InputBackground,
             shape = RoundedCornerShape(50)
@@ -114,8 +114,6 @@ fun InputText(
           onDone = {
             if (!setError.value) {
               action()
-              onValueChange("")
-              showInputDialog.value = false
             }
           }
         ),
